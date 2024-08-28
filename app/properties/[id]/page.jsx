638b2865c5cc884d175f1819da40c4c19d8fc2ac
kaloyanTry <kaloyantryavna@gmail.com>
2024-08-28 11:22:41 +0300

    Added Hero and Footer

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,5 @@
-const PropertyPage = () => {
-  return <div>Property id Page</div>;
+const PropertyPage = ({ params }) => {
+  return <div>Property id Page {params.id}</div>;
 };
 
 export default PropertyPage;
