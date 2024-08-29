4a52547cb764eb16841ef18bda333003363d6d54
kaloyanTry <kaloyantryavna@gmail.com>
2024-08-29 10:37:41 +0300

    Property Images and fetch data from MongoDB and Cloudinary

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,15 @@
 import Hero from '@/components/Hero';
+import HomeProperties from '@/components/HomeProperties';
 import InfoBoxes from '@/components/InfoBoxes';
+// import connectDB from '@/config/database';
 
 const HomePage = () => {
+  // connectDB();
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
