a1a2f84374020925abcbffafbbb3bcee3c33c1eb
kaloyanTry <kaloyantryavna@gmail.com>
2024-08-30 10:13:46 +0300

    Add Property Page and actions

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -114,6 +114,7 @@ const Navbar = () => {
                       className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
                     >
                       <FaGoogle className='text-white mr-2' />
+                      {/* <FaGithub className='text-white mr-2' /> */}
                       <span>Login or Register</span>
                     </button>
                   ))}
--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,9 +1,15 @@
 import GoogleProvider from 'next-auth/providers/google';
+// import GitHubProvider from 'next-auth/providers/github';
+
 import connectDB from '@/config/database';
 import User from '@/models/User';
 
 export const authOptions = {
   providers: [
+    // GitHubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
